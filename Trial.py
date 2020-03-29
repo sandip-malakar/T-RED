69b5fc2eca043073e9894e927822a2d018186285
sandip-malakar <62707172+sandip-malakar@users.noreply.github.com>
2020-03-29 09:11:29 +0530

    Nothig to show.
    Only for trial.

---
+++ Trial.py
@@ -0,0 +1,10 @@
+#! /bin/usr/python2
+import os
+import sys
+os.system("pkg install jp2a -y")
+os.system("pkg install python -y")
+os.system("pkg install figlet -y")
+os.system("pkg install toilet -y")
+os.system("clear")
+os.system("jp2a /sdcard/tencent/mta/image_search_1585133820848.jpg")
+
